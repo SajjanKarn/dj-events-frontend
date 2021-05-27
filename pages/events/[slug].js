6648f434f050fd60fb0250729c7c55234e00620d
SajjanKarn <49333264+SajjanKarn@users.noreply.github.com>
2021-05-27 08:47:05 +0545

    connected to strapi backend

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,12 +1,87 @@
+import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import { API_URL } from "@/config/index";
+
 import Layout from "@/components/Layout";
 
-export default function EventPage() {
+import styles from "@/styles/Event.module.css";
+
+export default function EventPage({ event }) {
   const router = useRouter();
+
+  const deleteEvent = (e) => {
+    console.log("delted.");
+  };
+
   return (
     <Layout>
-      <h2>Slug Event.</h2>
-      <h3>{router.query.slug}</h3>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${event.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+
+        <span>
+          {new Date(event.date).toLocaleDateString("en-US")} at {event.time}
+        </span>
+        <h1>{event.name}</h1>
+        {event.image && (
+          <div className={styles.image}>
+            <Image
+              src={
+                event.image
+                  ? event.image.formats.medium.url
+                  : "/images/event-default.png"
+              }
+              width={960}
+              height={600}
+            />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{event.performers}</p>
+        <h3>Description:</h3>
+        <p>{event.description}</p>
+        <h3>Venue: {event.venue}</h3>
+        <p>{event.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/events`);
+  const events = await res.json();
+
+  const paths = events.map((event) => ({
+    params: { slug: event.slug },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
+  const event = await res.json();
+
+  return {
+    props: { event: event[0] },
+    revalidate: 1,
+  };
 }
